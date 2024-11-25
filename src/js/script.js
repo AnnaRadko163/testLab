eaa509261056bf9bae0efb3a17c76922501f0d21
Anna <anna882882@mail.ru>
2024-11-25 18:14:36 +0400

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ import "../index.html";
 import '../sass/style.sass';
 import burger from "./modules/burger";
 import slider from "./modules/slider";
-import questions from "./modules/questions";
 import form from './modules/form';
+import questions from "./modules/questions";
 
 window.addEventListener('DOMContentLoaded', () => {
   burger()
